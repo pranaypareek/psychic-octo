84a967127490ef4dd01ebf5364eab5fb50c4d261
pranaypareek <pranay.metal@gmail.com>
2014-09-19 09:49:05 +0530

    update index.js

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res) {
   res.render('pages/index', { title: 'Here\'s logstuff1' });
 });
 
+router.get('/about', function(req, res){
+	res.render('pages/about');
+});
+
 module.exports = router;
